1061a34d5dbc50507a19f5529ed00c76f133b247
Olamide <olire96@gmail.com>
2024-08-14 17:22:48 +0100

    Updated database URL

--- app/db/session.py
+++ app/db/session.py
@@ -2,5 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
+if settings.DATABASE_URL.startswith("postgres://"):
+  settings.DATABASE_URL = settings.DATABASE_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
